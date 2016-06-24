7b7b1d0e423b5c840c418718a7c2eff8eddeb883
Victor Martins Machado <victormachado90@gmail.com>
2016-06-24 14:46:12 -0300

    Estrutura Inicial

---
+++ 0mod-base/both/route.js
@@ -0,0 +1,52 @@
+FlowRouter.notFound = {
+    action() {
+        BlazeLayout.render( 'default', { yield: 'notFound' } );
+    }
+};
+
+/* Public routes  */
+
+const publicRoutes = FlowRouter.group( { name: 'public' } );
+
+publicRoutes.route( '/', {
+    action() {
+        FlowRouter.go( '/login' );
+    }
+});
+
+publicRoutes.route( '/signup', {
+    name: 'signup',
+    action() {
+        BlazeLayout.render( 'default', { yield: 'signup' } );
+    }
+});
+
+publicRoutes.route( '/login', {
+    name: 'login',
+    action() {
+        BlazeLayout.render( 'default', { yield: 'login' } );
+    }
+});
+
+publicRoutes.route( '/recover-password', {
+    name: 'recover-password',
+    action() {
+        BlazeLayout.render( 'default', { yield: 'recoverPassword' } );
+    }
+});
+
+publicRoutes.route( '/reset-password/:token', {
+    name: 'reset-password',
+    action() {
+        BlazeLayout.render( 'default', { yield: 'resetPassword' } );
+    }
+});
+
+/* Public routes  */
+
+
+/* Authenticated routes  */
+
+const authenticatedRoutes = FlowRouter.group( { name: 'authenticated' } );
+
+/* Authenticated routes  */
