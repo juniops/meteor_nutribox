6dbc880202eb51915755c70f12421ee5ecbde7ed
Junio <junio.p@gmail.com>
2016-06-27 17:48:26 -0300

    layout menu

--- 0mod-base/client/helpers/template/date-time.js
+++ 0mod-base/client/helpers/template/date-time.js
@@ -1,21 +1,19 @@
-Template.registerHelper( 'formatDateTime', ( timestamp, format ) => {
-  if ( timestamp && format ) {
-    return moment( timestamp ).format( format );
-  }
+Template.registerHelper('formatDateTime', (timestamp, format) => {
+    if (timestamp && format) {
+        return moment(timestamp).format(format);
+    }
 });
 
-Template.registerHelper( 'formatDateTimeLocal', ( timestamp, timezone, format ) => {
-  if ( timestamp && timezone && format ) {
-    return moment( timestamp ).tz( timezone ).format( format );
-  }
+Template.registerHelper('formatDateTimeLocal', (timestamp, timezone, format) => {
+    if (timestamp && timezone && format) {
+        return moment(timestamp).tz(timezone).format(format);
+    }
 });
 
-Template.registerHelper( 'messageTimestamp', ( timestamp ) => {
-  if ( timestamp ) {
-    let today         = moment().format( 'L' ),
-        datestamp     = moment( timestamp ).format( 'L' ),
-        isBeforeToday = moment( today ).isAfter( datestamp ),
-        format        = isBeforeToday ? 'llll' : 'LT';
-    return moment( timestamp ).format( format );
-  }
+Template.registerHelper('messageTimestamp', (timestamp) => {
+    if (timestamp) {
+        isBeforeToday = moment(new Date()).isBefore(timestamp);
+        var format = isBeforeToday ? 'llll' : 'LT';
+        return moment(timestamp).format(format);
+    }
 });
--- 0mod-base/client/templates/globals/mainnav.js
+++ 0mod-base/client/templates/globals/mainnav.js
@@ -1,15 +1,18 @@
 
 Template.mainnav.rendered = function () {
+
+    import  "/0mod-base/imports/theme";
+    
     var menu = $('#mainnav-menu');
     if (menu.length) {
         $('#mainnav-menu').metisMenu({
             toggle: true
         });
-        // scrollbar = nifty.mainNav.find('.nano');
-        // if (scrollbar.length) {
-        //     scrollbar.nanoScroller({
-        //         preventPageScrolling: true
-        //     });
-        // }
+        scrollbar = nifty.mainNav.find('.nano');
+        if (scrollbar.length) {
+            scrollbar.nanoScroller({
+                preventPageScrolling: true
+            });
+        }
     }
 };
